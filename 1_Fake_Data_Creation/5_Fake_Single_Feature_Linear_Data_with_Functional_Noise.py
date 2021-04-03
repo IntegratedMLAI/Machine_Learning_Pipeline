b11fcb781a2b18703331c7356eff28bbfbb938a1
ThomIves <thom.ives@gmail.com>
2021-04-02 20:36:06 -0600

    added some variable noise functions

---
+++ 1_Fake_Data_Creation/5_Fake_Single_Feature_Linear_Data_with_Functional_Noise.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import random
+import math
+
+
+def my_f(x, max):
+    # return x * max
+    return math.sin(1.5 * x) * max
+
+
+num_pts = 1000
+divider = num_pts / 10
+X = [x/divider for x in range(num_pts)]
+Y = [2.0 * x + (random.random() - 0.5) * my_f(x, 10) for x in X]
+
+plt.scatter(X, Y)
+plt.title('This Is The Title')
+plt.xlabel('These Are The X Values')
+plt.ylabel('These Are The Y Values')
+plt.show()
+
+with open('./1_Fake_Data_Creation/linear_data_var_noise.csv', 'w') as f:
+    for i in range(len(X)):
+        this_line = f'{X[i]}, {Y[i]}\n'
+        f.write(this_line)
